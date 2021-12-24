9b2a993d8b84e0c39ce052e7e7090ca0982b04db
Harald Gutsche <hg42@gmx.net>
2021-12-24 13:11:57 +0100

    unsuspend: snackbar instead of toast

--- app/src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
+++ app/src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
@@ -332,6 +332,16 @@ class MainActivityX : BaseActivity() {
     fun dismissSnackBar() {
         binding.snackbarText.visibility = View.GONE
     }
+
+    fun whileShowingSnackBar(message: String, todo: () -> Unit) {
+        MainActivityX.activity?.runOnUiThread {
+            MainActivityX.activity?.showSnackBar("cleanup any left over suspended apps")
+        }
+        todo()
+        MainActivityX.activity?.runOnUiThread {
+            MainActivityX.activity?.dismissSnackBar()
+        }
+    }
 }
 
 
