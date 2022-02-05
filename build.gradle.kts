53e6d8f2a69eb5a1bc0110ee724090f006bf327b
Harald Gutsche <hg42@gmx.net>
2022-02-05 17:57:08 +0100

    LOCAL hg42, apkname, 🐞debug, 🎃test, signing, gitversion, colors, todo

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,15 +15,21 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 buildscript {
     repositories {
         mavenCentral()
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("com.android.tools.build:gradle:7.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0-alpha01")
     }
+}
+
+plugins {
+    id("com.palantir.git-version") version "0.12.3"
 }
 
 allprojects {
@@ -37,3 +43,52 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+
+//println("============================================== version")
+
+//val gitVersion: groovy.lang.Closure<String> by extra
+//val gitversion = gitVersion()
+//println("gitversion: $gitversion")
+
+val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+
+val gitdetails = versionDetails()
+//println("gitdetails: $gitdetails")
+val commit = gitdetails.gitHash.take(8)
+
+val refTime = java.util.GregorianCalendar(2020,0,1).time // Date
+val startTime = java.util.Date()
+//val seconds = ((startTime.time - refTime.time) / 1000)
+//println("seconds:   $seconds")
+//val hours = (seconds / 60 / 60).toLong()
+//println("hours:     $hours")
+
+var buildTime    = java.text.SimpleDateFormat("yyMMddHHmmss").format(startTime)
+var buildNumber  = java.text.SimpleDateFormat("yyDDDHHmm").format(startTime).substring(1, 7)
+var buildLabel   = gitdetails.branchName.replace("feature-", "").replace("PR-", "")
+var buildVersion = "${buildNumber}-${buildLabel}-${gitdetails.commitDistance}-${commit}"
+//                           + (gitdetails.isCleanTag ? "" : "-DIRTY")
+
+rootProject.ext.set("buildNumber",  buildNumber)
+rootProject.ext.set("buildVersion", buildVersion)
+
+/*
+println("""
+version palantir:
+    commitDistance: ${gitdetails.commitDistance}
+    lastTag:        ${gitdetails.lastTag}
+    gitHash:        ${gitdetails.gitHash}
+    gitHashFull:    ${gitdetails.gitHashFull}
+    branchName:     ${gitdetails.branchName}
+    isCleanTag:     ${gitdetails.isCleanTag}
+""")
+*/
+println("""
+version build:
+    startTime:      $startTime
+    buildTime:      $buildTime
+    buildVersion:   $buildVersion
+""")
+
+//println("============================================== version.")
