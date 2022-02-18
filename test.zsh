d1877fa6dd2bea00742fd618f36212278c09500a
Harald Gutsche <hg42@gmx.net>
2022-02-18 18:10:18 +0100

    pump up test.zsh

--- test.zsh
+++ test.zsh
@@ -1,30 +1,56 @@
 #!/bin/zsh
 
-start=${1:-3}
+start=3
+step=1
+variant=hg42.debug
+  schedules=(
+    test9_1
+    test9_2
+    test9_3
+    test9_4
+    test9_5
+    test9_6
+    test9_7
+    test9_8
+    special
+    user
+    system
+    )
+debug=
+
+if [[ $1 == -d ]]; then
+  debug=echo
+  shift
+  set -x
+fi
+
+if [[ -n $1 ]]; then
+  start=$1
+  shift
+fi
+
+if [[ -n $1 ]]; then
+  step=$1
+  shift
+fi
+
+if [[ -n $1 ]]; then
+  variant=$1
+  shift
+fi
+
+if [[ -n $* ]]; then
+  schedules=($*)
+fi
+
+step=$(($step*60))
 if [[ $start != *:* ]]; then
   start=$( date +%H:%M -d @$(( $(date +%s) + $(($start*60)) )) )
 fi
 
-step=${2:-1}
-step=$(($step*60))
-
-variant=${3:-hg42.debug}
-
 time=$start
 
-for schedule in \
-    test9_1     \
-    test9_2     \
-    test9_3     \
-    test9_4     \
-    test9_5     \
-    test9_6     \
-    test9_7     \
-    test9_8     \
-    special     \
-    user        \
-    system      \
-    ; do
-  adbsu am broadcast -a reschedule -e name $schedule -e time $time -n com.machiav3lli.backup.$variant/com.machiav3lli.backup.services.CommandReceiver
+for schedule in $schedules; do
+  $debug adbsu am broadcast -a reschedule -e name $schedule -e time $time -n com.machiav3lli.backup.$variant/com.machiav3lli.backup.services.CommandReceiver
   time=$( date +%H:%M -d @$(( $(date +%s -d $time) + $step )) )
 done
